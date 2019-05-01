aa52567e314c12d84031d5a93c101078988256f3
Ruby Tazim <rubaiattazim@gmail.com>
2019-04-30 22:42:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const express = require('express'); //import express library
 const app = express(); //assign to variable (express)
 
 app.use(express.static('static_files')); //so app can serve static files
+
+app.get('/createprofile', (req, res) => {
+  
+});
+
+app.get('/createschool', (req, res) => {
+  
+});
